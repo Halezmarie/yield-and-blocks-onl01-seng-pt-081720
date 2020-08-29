f4a87f633ec6f72f7bcfc052348e8c62c1f816fe
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-29 23:38:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,19 @@
-require 'pry'
+def hello_t(array)
+   if block_given? #check if there is a block in calling method
+      i = 0
+   while i < array.length
+      yield array[i]
+      i += 1 #looping through each index
+   end
+   array #return the final array
+   else
+      puts "Hey! No block was given!"
+   end
+end
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
+#call the method
+hello_t(["Tim", "Tom", "Jim"]) do |name| #parameter for array[i] from yield
+   if name.start_with?("T")
+      puts "Hi, #{name}"
+   end
 end
